37fee870b1fc4c4804bff8ba5c2b430061b43713
Scott_tsao <o60816@gmail.com>
2018-12-09 12:16:59 +0800

    Front end design finished

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ type project struct {
 
 type work struct {
 	Period      string    `json:"period"`
+	Logo        string    `json:"Logo"`
 	Company     string    `json:"company"`
 	Position    string    `json:"position"`
 	Content     string    `json:"content"`
@@ -29,8 +30,15 @@ var projectList = []project{
 	project{ProjectName: "企業協作平台", Tech: "Soap、muti-thread、傳送文字、語音、圖片"},
 }
 
+var projectList2 = []project{
+	project{ProjectName: "移植測試framework從Perl到Python", Tech: "Perl、Python"},
+	project{ProjectName: "完成多個測試程式的撰寫", Tech: "Python、Selenium、架設server"},
+	project{ProjectName: "Fake browser", Tech: "熟悉網路傳輸協定(TLS,HTTP,HTTP2,SOCKS)、c++、windows api、閱讀chrome與firefox source code、windbg。"},
+}
+
 var workList = []work{
-	work{Period: "2013/10~2016/4", Company: "海南金海漿紙業有限公司", Position: "軟體開發工程師", Content: "工作內容:\n開發android、ios兩大移動平台的公司入口應用程式並將桌面平台的系統移植、整合到其中。", ProjectList: projectList},
+	work{Period: "Oct 2013-Apr 2016", Logo: "http://www.ccichain.net/images/up_images/2014420114640.bmp", Company: "海南金海漿紙業有限公司", Position: "軟體開發工程師", Content: "開發android、ios兩大移動平台的公司入口應用程式並將桌面平台的系統移植、整合到其中。", ProjectList: projectList},
+	work{Period: "May 2016~Present", Logo: "https://www.trendmicro.com/content/dam/trendmicro/global/en/global/logo/trend-micro-mobile.png", Company: "趨勢科技", Position: "軟體測試工程師", Content: "撰寫測試計畫、測試程式、測試工具，維護測試framework，架設測試伺服器，分析與解決客戶問題。", ProjectList: projectList2},
 }
 
 func main() {
