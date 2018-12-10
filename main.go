2374d7b951a78590775facdc46ec588ef5e8ce53
Scott_tsao <o60816@gmail.com>
2018-12-10 23:11:59 +0800

    Add two features: create work experience and delete work experience

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -28,23 +29,6 @@ type Work struct {
 	Content     string    `json:"content"`
 	ProjectList []Project `json:"ProjectList"`
 }
-
-// var projectList = []Project{
-// 	Project{ProjectName: "手機遠端查看並控制攝影機", Tech: "QT  framework、 c++、javascript、ffmpeg、darwin  streaming server、rtsp、onvif、muti-thread"},
-// 	Project{ProjectName: "手機遠端即時查看設備與生產資訊", Tech: "OPC  Server,web  service、soap、muti-thread"},
-// 	Project{ProjectName: "企業協作平台", Tech: "Soap、muti-thread、傳送文字、語音、圖片"},
-// }
-
-// var projectList2 = []Project{
-// 	Project{ProjectName: "移植測試framework從Perl到Python", Tech: "Perl、Python"},
-// 	Project{ProjectName: "完成多個測試程式的撰寫", Tech: "Python、Selenium、架設server"},
-// 	Project{ProjectName: "Fake browser", Tech: "TLS、HTTP、HTTP2、SOCKS、c++、windows api、閱讀chrome與firefox source code、windbg"},
-// }
-
-// var workList = []work{
-// 	work{Period: "Oct 2013-Apr 2016", Logo: "http://www.ccichain.net/images/up_images/2014420114640.bmp", Company: "海南金海漿紙業有限公司", Position: "軟體開發工程師", Content: "開發android、ios兩大移動平台的公司入口應用程式並將桌面平台的系統移植、整合到其中", ProjectList: projectList},
-// 	work{Period: "May 2016~Present", Logo: "https://www.trendmicro.com/content/dam/trendmicro/global/en/global/logo/trend-micro-mobile.png", Company: "趨勢科技", Position: "軟體測試工程師", Content: "撰寫測試計畫、測試程式、測試工具、維護測試framework、架設測試伺服器、分析與解決客戶問題", ProjectList: projectList2},
-// }
 
 func main() {
 	db, err := sql.Open("mysql", "root:12345678@tcp(127.0.0.1:3306)/Resume?parseTime=true")
@@ -109,6 +93,49 @@ func main() {
 				"works": workList,
 			},
 		)
+	})
+
+	router.GET("/create", func(c *gin.Context) {
+		c.HTML(
+			http.StatusOK,
+			"addWork.html",
+			gin.H{},
+		)
+	})
+
+	router.POST("/create", func(c *gin.Context) {
+		result, err := db.Exec("INSERT INTO work(period, logo, company, position, content) VALUES(?,?,?,?,?)", c.PostForm("period"), c.PostForm("logo"), c.PostForm("company"), c.PostForm("position"), c.PostForm("content"))
+		if err != nil {
+			log.Fatal(err)
+		}
+		rows, err := result.RowsAffected()
+		if err != nil {
+			log.Fatal(err)
+		}
+		if rows != 1 {
+			panic(err)
+		}
+		c.Redirect(http.StatusMovedPermanently, "http://localhost:8080/")
+	})
+
+	router.GET("/delete/:workId", func(c *gin.Context) {
+		if workID, err := strconv.Atoi(c.Param("workId")); err == nil {
+			result, err := db.Exec("DELETE FROM work WHERE id=?", workID)
+			if err != nil {
+				log.Fatal(err)
+			}
+			rows, err := result.RowsAffected()
+			if err != nil {
+				log.Fatal(err)
+			}
+			if rows != 1 {
+				panic(err)
+			}
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+
+		c.Redirect(http.StatusMovedPermanently, "http://localhost:8080/")
 	})
 
 	router.Run(":8080")
